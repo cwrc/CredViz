be5d8158f1903bbda095da29ac6ebd4d835fd800
Robin Miller <robin@tenjin.ca>
2016-09-09 11:56:02 -0600

    Cleaned up graph code into an object orientation

--- faces/web/assets/javascripts/components/credit_visualization.js
+++ faces/web/assets/javascripts/components/credit_visualization.js
@@ -9,125 +9,7 @@ ko.components.register('credit_visualization', {
       // STATE
       self.data = ko.observableArray();
 
-      self.totalChanges = function (datum) {
-         var total = 0;
-
-         for (var type in datum.changes)
-            total += datum.changes[type];
-
-         return total;
-      };
-
-      self.initD3 = function () {
-         var svg, margin, width, height, svgGroup, usersScale, contributionScale, colorScale, svgGroupVM;
-         var stack = d3.stack();
-         var legend, legendVM, data;
-
-         svg = d3.select("svg");
-         margin = {top: 20, right: 20, bottom: 30, left: 40};
-         width = +svg.attr("width") - margin.left - margin.right;
-         height = +svg.attr("height") - margin.top - margin.bottom;
-         svgGroup = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-         usersScale = d3.scaleBand()
-            .rangeRound([0, width])
-            .padding(0.1)
-            .align(0.1);
-
-         contributionScale = d3.scaleLinear()
-            .rangeRound([height, 0]);
-
-         colorScale = d3.scaleOrdinal()
-            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-         data = self.data();
-
-         data.sort(function (a, b) {
-            return self.totalChanges(b) - self.totalChanges(a)
-         });
-
-         var workTypes = ['write', 'edit'];
-
-         usersScale.domain(data.map(function (d) {
-            return d.user.name;
-         }));
-         contributionScale.domain([0, d3.max(data, function (d) {
-            return self.totalChanges(d);
-         })]).nice();
-         colorScale.domain(workTypes); // TODO: dynamically set this based on the workflow keys
-
-         var stacker = stack.keys(workTypes)
-            .value(function (datum, key) {
-               return datum.changes[key];
-            });
-
-         // create one group for each work type
-         svgGroupVM = svgGroup.selectAll(".serie").data(stacker(data));
-
-         svgGroupVM.enter().append("g")
-            .attr("class", "serie")
-            .attr("fill", function (d) {
-               return colorScale(d.key);
-            })
-            .selectAll("rect")
-            .data(function (d) {
-               return d;
-            })
-            .enter().append("rect")
-            .attr("x", function (d) {
-               return usersScale(d.data.user.name);
-            })
-            .attr("y", function (d) {
-               return contributionScale(d[1]);
-            })
-            .attr("height", function (d) {
-               return contributionScale(d[0]) - contributionScale(d[1]);
-            })
-            .attr("width", usersScale.bandwidth());
-
-         // Bottom Scale
-         svgGroup.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(usersScale));
-
-         // Left Scale
-         svgGroup.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(contributionScale).ticks(10, "s"))
-            .append("text")
-            .attr("x", 2)
-            .attr("y", contributionScale(contributionScale.ticks(10).pop()))
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "start")
-            .attr("fill", "#000")
-            .text("Contribution");
-
-
-         legend = svgGroup.selectAll(".legend")
-            .data(workTypes.reverse())
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d, i) {
-               return "translate(0," + i * 20 + ")";
-            })
-            .style("font", "10px sans-serif");
-
-         legend.append("rect")
-            .attr("x", width - 18)
-            .attr("width", 18)
-            .attr("height", 18)
-            .attr("fill", colorScale);
-
-         legend.append("text")
-            .attr("x", width - 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .attr("text-anchor", "end")
-            .text(function (d) {
-               return d.toUpperCase()[0] + d.slice(1);
-            });
-      };
+      self.grapher = new CWRC.CreditVisualization.StackedColumnGraph(self.data);
 
       self.getWorkData = function (id) {
          //var projects = [{
@@ -181,7 +63,6 @@ ko.components.register('credit_visualization', {
             }
          ]);
 
-
          // TODO: actually call the server
          //ajax('post', '/users/' + id, '', function (response) {
          //   self.data(response);
@@ -189,14 +70,157 @@ ko.components.register('credit_visualization', {
       };
 
       self.getWorkData();
-      self.initD3();
+      self.grapher.render();
    }
 });
 
+var CWRC = CWRC || {};
+CWRC.CreditVisualization = CWRC.CreditVisualization || {};
+
 (function () {
-   function StackedColumnGraph(dataSetOservable) {
-      this.data = dataSetOservable();
-   }
+   CWRC.CreditVisualization.StackedColumnGraph = function (dataSetOservable) {
+      var self = this;
 
+      var svg = d3.select("svg");
 
+      this.data = dataSetOservable;
+
+      this.bounds = {
+         margin: {top: 20, right: 20, bottom: 30, left: 30},
+         getInnerWidth: function () {
+            return +svg.attr("width") - self.bounds.margin.left - self.bounds.margin.right;
+         },
+         getInnerHeight: function () {
+            return +svg.attr("height") - self.bounds.margin.top - self.bounds.margin.bottom;
+         }
+      };
+      this.contentGroup = svg.append("g").attr("transform", "translate(" + this.bounds.margin.left + "," + this.bounds.margin.top + ")");
+
+      this.usersScale = d3.scaleBand()
+         .rangeRound([0, this.bounds.getInnerWidth()])
+         .padding(0.1)
+         .align(0.1);
+
+      this.contributionScale = d3.scaleLinear()
+         .rangeRound([this.bounds.getInnerHeight(), 0]);
+
+      this.colorScale = d3.scaleOrdinal()
+         .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+   };
+
+   CWRC.CreditVisualization.StackedColumnGraph.prototype.render = function () {
+      var self = this;
+
+      var data, contentGroupVM, workTypeStacker, workTypes;
+
+      workTypes = ['write', 'edit']; // TODO: dynamically determine this based on the workflow keys
+
+      data = self.data();
+
+      data.sort(function (a, b) {
+         return self.totalChanges(b) - self.totalChanges(a)
+      });
+
+      self.usersScale.domain(data.map(function (d) {
+         return d.user.name;
+      }));
+      self.contributionScale.domain([0, d3.max(data, function (d) {
+         return self.totalChanges(d);
+      })]).nice();
+      self.colorScale.domain(workTypes);
+
+      workTypeStacker = d3.stack().keys(workTypes)
+         .value(function (datum, key) {
+            return datum.changes[key];
+         });
+
+      // create one group for each work type
+      contentGroupVM = self.contentGroup.selectAll(".serie").data(workTypeStacker(data));
+
+      contentGroupVM.enter().append("g")
+         .attr("class", "serie")
+         .attr("fill", function (d) {
+            return self.colorScale(d.key);
+         })
+         .selectAll("rect")
+         .data(function (d) {
+            return d;
+         })
+         .enter().append("rect")
+         .attr("x", function (d) {
+            return self.usersScale(d.data.user.name);
+         })
+         .attr("y", function (d) {
+            return self.contributionScale(d[1]);
+         })
+         .attr("height", function (d) {
+            return self.contributionScale(d[0]) - self.contributionScale(d[1]);
+         })
+         .attr("width", self.usersScale.bandwidth());
+
+      this.constructBottomScale(workTypes);
+      this.constructLeftScale(workTypes);
+      this.constructLegend(workTypes);
+   };
+
+   CWRC.CreditVisualization.StackedColumnGraph.prototype.constructBottomScale = function (workTypes) {
+      var self = this;
+
+      self.contentGroup.append("g")
+         .attr("class", "axis axis--x")
+         .attr("transform", "translate(0," + this.bounds.getInnerHeight() + ")")
+         .call(d3.axisBottom(this.usersScale));
+   };
+
+   CWRC.CreditVisualization.StackedColumnGraph.prototype.constructLeftScale = function () {
+      var self = this;
+
+      self.contentGroup.append("g")
+         .attr("class", "axis axis--y")
+         .call(d3.axisLeft(self.contributionScale).ticks(10, "s"))
+         .append("text")
+         .attr("x", 2)
+         .attr("y", self.contributionScale(self.contributionScale.ticks(10).pop()))
+         .attr("dy", "0.35em")
+         .attr("text-anchor", "start")
+         .attr("fill", "#000")
+         .text("Contribution");
+   };
+
+   CWRC.CreditVisualization.StackedColumnGraph.prototype.constructLegend = function (workTypes) {
+      var self = this, legend;
+
+      legend = self.contentGroup.selectAll(".legend")
+         .data(workTypes.reverse())
+         .enter().append("g")
+         .attr("class", "legend")
+         .attr("transform", function (d, i) {
+            return "translate(0," + i * 20 + ")";
+         })
+         .style("font", "10px sans-serif");
+
+      legend.append("rect")
+         .attr("x", self.bounds.getInnerWidth() - 18)
+         .attr("width", 18)
+         .attr("height", 18)
+         .attr("fill", this.colorScale);
+
+      legend.append("text")
+         .attr("x", self.bounds.getInnerWidth() - 24)
+         .attr("y", 9)
+         .attr("dy", ".35em")
+         .attr("text-anchor", "end")
+         .text(function (columnName) {
+            return columnName.toUpperCase()[0] + columnName.slice(1);
+         });
+   };
+
+   CWRC.CreditVisualization.StackedColumnGraph.prototype.totalChanges = function (datum) {
+      var total = 0;
+
+      for (var type in datum.changes)
+         total += datum.changes[type];
+
+      return total;
+   };
 })();
