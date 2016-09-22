b0a8c67b1f90fe690b29df0ef43abcbaaf535400
Robin Miller <robin@tenjin.ca>
2016-09-22 17:54:24 -0600

    Made user totals show when labels are hovered

--- assets/javascripts/components/credit_visualization.js
+++ assets/javascripts/components/credit_visualization.js
@@ -228,6 +228,38 @@ CWRC.CreditVisualization = CWRC.CreditVisualization || {};
             return baseline + (top - self.contributionScale(dataRow[0])) / 2;
          });
 
+      maxValue = d3.max(workTagStack.reduce(function (a, b) {
+         return a.concat(b.reduce(function (c, d) {
+            return c.concat(d);
+         }, []));
+      }, []));
+
+
+      // per-user maxiumums
+      self.contentGroup.selectAll('.total-labels')
+         .data(data)
+         .enter()
+         .append('g')
+         .attr("class", function (datum) {
+            return "total-labels total-label-user-" + datum.user.id;
+         })
+         .append("text")
+         .text(function (d) {
+            return percentFormat(self.countChanges(d) / allChangesCount);
+         })
+         .attr('x', function (d) {
+            return self.usersScale(JSON.stringify(d.user)) + self.usersScale.bandwidth() / 2;
+         })
+         .attr('y', function (datum, index) {
+            var finalStackRow, userSegment, segmentTop;
+
+            // each stack row is all segments within a category, so last one is top
+            finalStackRow = workTagStack[workTagStack.length - 1];
+            userSegment = finalStackRow[index]; // the value pair for this column
+            segmentTop = userSegment[1];
+
+            return self.contributionScale(segmentTop) - 4;
+         });
 
       this.constructBottomScale();
       this.constructLeftScale();
@@ -281,7 +313,7 @@ CWRC.CreditVisualization = CWRC.CreditVisualization || {};
    CWRC.CreditVisualization.StackedColumnGraph.prototype.constructBottomScale = function () {
       var self = this;
 
-      var axis, tickGroup, existingTickLabels, tickFill, tickX, tickY, tickDY, columnWidth;
+      var axis, tickGroup, existingTickLabels, tickFill, tickX, tickY, tickDY, columnWidth, userLabelHoverHandler;
 
       axis = d3.axisBottom(self.usersScale)
          .tickFormat(function (datum) {
@@ -304,6 +336,18 @@ CWRC.CreditVisualization = CWRC.CreditVisualization || {};
       tickDY = existingTickLabels.attr('dy');
       existingTickLabels.remove();
 
+      userLabelHoverHandler = function () {
+         var user, isEnter;
+
+         user = JSON.parse(d3.select(this.parentNode).datum());
+
+         isEnter = d3.event.type == 'mouseover';
+
+         self.svg.select('.total-label-user-' + user.id)
+            .classed('highlight', isEnter);
+      };
+
+
       tickGroup.selectAll('.tick')
          .append('a')
          .attr('xlink:href', function (datum) {
@@ -320,7 +364,10 @@ CWRC.CreditVisualization = CWRC.CreditVisualization || {};
             var user = JSON.parse(datum);
 
             return user.name
-         });
+         })
+         .on('mouseover', userLabelHoverHandler)
+         .on('mouseout', userLabelHoverHandler);
+
 
       columnWidth = self.usersScale.bandwidth(); // labelling for clarity
 
