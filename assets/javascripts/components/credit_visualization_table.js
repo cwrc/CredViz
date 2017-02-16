08b8fbd282cf2f7d3db6231bdf2292f498c74cc0
Robin Miller <robin@tenjin.ca>
2017-02-15 18:22:39 -0700

    Basic table support done.

--- assets/javascripts/components/credit_visualization_table.js
+++ assets/javascripts/components/credit_visualization_table.js
@@ -8,11 +8,11 @@ ko.components.register('credit-visualization-table', {
                         <!-- /ko -->\
                      </tr>\
                   </thead>\
-                  <tbody data-bind="foreach: workTypes">\
+                  <tbody data-bind="foreach: {data: workTypes, as: \'workType\'}">\
                      <tr>\
-                        <th data-bind="text: $parent.cleanLabel($data)"></th>\
-                        <!-- ko foreach: $parent.users -->\
-                           <td>%</td>\
+                        <th data-bind="text: $parent.cleanLabel(workType)"></th>\
+                        <!-- ko foreach: {data: $parent.users, as: \'user\' } -->\
+                           <td data-bind="text: $parents[1].getContributionForType(user, workType)"></td>\
                         <!-- /ko -->\
                      </tr>\
                   </tbody>\
@@ -21,11 +21,8 @@ ko.components.register('credit-visualization-table', {
    viewModel: function (params) {
       var self = this;
 
-      self.data = params.data;
-
-      self.data.subscribe(function (data) {
-         console.log(data)
-      });
+      self.data = params.filteredModifications;
+      self.totalNumChanges = params.totalNumChanges;
 
       self.users = ko.pureComputed(function () {
          return self.data().map(function (d) {
@@ -37,5 +34,20 @@ ko.components.register('credit-visualization-table', {
       self.cleanLabel = function (workType) {
          return CWRC.toTitleCase(workType.replace('_', ' '));
       };
+
+      self.getContributionForType = function (user, workType) {
+         var datum, percentage;
+
+         datum = self.data().find(function (d) {
+            return d.user.id == user.id;
+         });
+
+         var nDecimals = 1;
+         var decimalShifter = Math.pow(10, nDecimals);
+
+         percentage = Math.round(((datum.workflow_changes[workType] || 0) / self.totalNumChanges() ) * 100 * decimalShifter);
+
+         return percentage / decimalShifter + '%';
+      }
    }
 });
